20ef827574a52cc49b893c0a867ad576320fbeaa
Sam Fortin <sam.j.fortin@gmail.com>
2017-07-31 13:26:12 -0500

    Add button clicked counter and added to DOM. Base mode #3

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+
 $(document).ready(function () {
-    console.log('script and jquery sourced');
+    // console.log('script and jquery sourced');
+
+    // Global variables
+    var buttonClickCounter = 0;
 
     // Event handler for generate button
     $('#generate').on('click', function(){
-        $('#container').append('<div>Test</div>');
+        buttonClickCounter++;
+        $('#container').append('<div><p class="buttonCounter">' + buttonClickCounter + '</p></div>');
     });
 });
