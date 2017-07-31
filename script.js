0425d8b453f2e923adc0abe9c3c61d92319a4577
Sam Fortin <sam.j.fortin@gmail.com>
2017-07-31 13:50:59 -0500

    Organize code and added comments

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
+// Global variables
+var buttonClickCounter = 0;
+
 $(document).ready(function () {
     // console.log('script and jquery sourced');
 
-    // Global variables
-    var buttonClickCounter = 0;
-
-    // Event handler for generate button
-    $('#container').on('click', '#generate', function () {
-        buttonClickCounter++;
-        $('#container').append('<div class="generated">' +
-            '<p class="buttonCounter">' +
-            buttonClickCounter +
-            '</p>' +
-            '<button class="swap">Swap</button>' +
-            '<button class="delete">Delete</button>' +
-            '</div>');
-    });
-
-    $('#container').on('click', '.delete', function () {
-        ($(this)).parent().remove();
-    });
-
-    $('#container').on('click', '.swap', function () {
-        ($(this)).parent().toggleClass('yellow');
-    });
+    // Event handlers
+    $('#container').on('click', '#generate', generateDiv);
+    $('#container').on('click', '.delete', removeDiv);
+    $('#container').on('click', '.swap', swapColor);
 });
+
+// Generate div
+function generateDiv() {
+    buttonClickCounter++;
+    $('#container').append('<div class="generated">' +
+        '<p class="buttonCounter">' +
+        buttonClickCounter +
+        '</p>' +
+        '<button class="swap">Swap</button>' +
+        '<button class="delete">Delete</button>' +
+        '</div>');
+}
+
+// Remove div when its child delete button is clicked
+function removeDiv() {
+    ($(this)).parent().remove();
+}
+
+// Toggle div background color when its child swap button is clicked
+function swapColor() {
+    ($(this)).parent().toggleClass('yellow');
+}
