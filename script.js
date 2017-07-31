b1b9153b44e5385af754d80417b7e88a71e9af42
Sam Fortin <sam.j.fortin@gmail.com>
2017-07-31 13:21:25 -0500

    Generate button event handler added. Added append div to DOM

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
     console.log('script and jquery sourced');
-    
+
+    // Event handler for generate button
+    $('#generate').on('click', function(){
+        $('#container').append('<div>Test</div>');
+    });
 });
